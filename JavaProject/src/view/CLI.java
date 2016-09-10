5b34d7e3adace629563843b773fc11e78262027b
Yoni <Yoni@DESKTOP-6J4RMC7>
2016-09-10 16:27:51 +0300

    added CLI

--- JavaProject/src/view/CLI.java
+++ JavaProject/src/view/CLI.java
@@ -1,9 +1,38 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class CLI {
-
-	public CLI() {
-		
+	private BufferedReader in;
+	private PrintWriter out;
+	private static final String EXITKEY = "exit";
+	
+	public CLI(BufferedReader in, PrintWriter out) {
+		this.out = out;
+		this.in = in;
 	}
+	public void start() throws IOException {
+		String choice = in.readLine();
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				String choice = null;
+				try {
+					choice = in.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				while (!choice.equals(EXITKEY)) {
+					
+					
+				}
+			}
+		});
+		
+		t.start();
 
+	}
 }
