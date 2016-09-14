4ada434ad03a39a1c2fd210147a0ce8bffa42239
eliran <eliran@Elirana-lp.spiderservices.com>
2016-09-14 20:38:13 +0300

    View package

--- JavaProject/src/view/View.java
+++ JavaProject/src/view/View.java
@@ -1,5 +1,17 @@
 package view;
 
-public interface View {
+import java.util.HashMap;
 
+import algorithms.mazeGenerators.Maze3d;
+import controller.Command;
+import controller.Controller;
+
+public interface View {
+	
+	void setController(Controller c);
+	void setCommands(HashMap<String, Command> commands);
+	void notifyMazeIsReady(String name);
+	void displayMaze(Maze3d maze);
+	void start();
+	
 }
